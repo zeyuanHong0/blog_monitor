51d8d67e60fa85ab3ecd04e89d4e7722770a8e4c
Alexander Smith <hongzeyuan0@gmail.com>
2026-05-11 21:23:03 +0800

    feat(collect): 更新收集控制器和服务，移除未使用的方法并添加报告功能

--- packages/server/src/collect/collect.controller.ts
+++ packages/server/src/collect/collect.controller.ts
@@ -1,22 +1,12 @@
-import { Controller, Get, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Body, Param, Delete, Post } from '@nestjs/common';
 import { CollectService } from './collect.service';
 
 @Controller('collect')
 export class CollectController {
   constructor(private readonly collectService: CollectService) {}
 
-  @Get()
-  findAll() {
-    return this.collectService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.collectService.findOne(+id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.collectService.remove(+id);
+  @Post('report')
+  report(@Body() reportData: any) {
+    return this.collectService.report(reportData);
   }
 }
--- packages/server/src/collect/collect.service.ts
+++ packages/server/src/collect/collect.service.ts
@@ -2,15 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CollectService {
-  findAll() {
-    return `This action returns all collect`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} collect`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} collect`;
+  report(reportData: any) {
+    console.log('Received report data:', reportData);
   }
 }
