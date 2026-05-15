a91ee9fdc9df1da5442c4c917ef7cbd687e93f85
Alexander Smith <hongzeyuan0@gmail.com>
2026-05-15 21:42:32 +0800

    feat(collect): 新增多种数据收集接口

--- packages/server/src/collect/collect.controller.ts
+++ packages/server/src/collect/collect.controller.ts
@@ -1,14 +1,40 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Ip, Post } from '@nestjs/common';
 
 import { CollectService } from './collect.service';
 import { ReportPayloadDto } from './dto/reportPayload.dto';
+import { EventDto } from './dto/event.dto';
+import { ErrorDto } from './dto/error.dto';
+import { SoftNavigationDto } from './dto/softNavigation.dto';
+import { PerformanceDto } from './dto/performance.dto';
+import { PageViewDto } from './dto/pageview.dto';
 
 @Controller('collect')
 export class CollectController {
   constructor(private readonly collectService: CollectService) {}
 
-  @Post('report')
-  report(@Body() reportData: ReportPayloadDto) {
-    return this.collectService.report(reportData);
+  @Post('batch')
+  report(@Body() reportData: ReportPayloadDto, @Ip() ip: string) {
+    return this.collectService.processBatch(reportData.data, ip);
+  }
+
+  @Post('pageview')
+  pageview(@Body() pageviewData: PageViewDto, @Ip() ip: string) {
+    return this.collectService.savePageView(pageviewData, ip);
+  }
+  @Post('performance')
+  performance(@Body() performanceData: PerformanceDto) {
+    return this.collectService.savePerformance(performanceData);
+  }
+  @Post('soft_navigation')
+  softNavigation(@Body() softNavigationData: SoftNavigationDto) {
+    return this.collectService.saveSoftNavigation(softNavigationData);
+  }
+  @Post('error')
+  error(@Body() errorData: ErrorDto) {
+    return this.collectService.saveError(errorData);
+  }
+  @Post('event')
+  event(@Body() eventData: EventDto) {
+    return this.collectService.saveEvent(eventData);
   }
 }
